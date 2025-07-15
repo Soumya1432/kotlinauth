b57aa3ed8dce674cde48780c0bd748b84a410553
IamSoumyaBiswas <imsoumyabiswas@gmail.com>
2025-07-15 19:10:06 +0530

    text

--- app/src/main/java/com/example/practicekotlin/MainActivity.kt
+++ app/src/main/java/com/example/practicekotlin/MainActivity.kt
@@ -1,36 +1,56 @@
 package com.example.practicekotlin
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.W200
+import androidx.compose.ui.text.font.FontWeight.Companion.W500
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practicekotlin.ui.theme.PracticekotlinTheme
 
 class MainActivity : ComponentActivity() {
-
     var sendIntent = Intent(Intent.ACTION_SEND)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // change the status bar color here
         window.statusBarColor = android.graphics.Color.parseColor("#6A11CB")
         setContent {
             PracticekotlinTheme {
-                LoginScreen()
+                LoginScreen({})
             }
         }
     }
 }
 
+//@Composable
+//fun FilledButtonExample(onClick: () -> Unit) {
+//    Button { }(onClick = { onClick() }) {
+//        Text("Filled")
+//    }
+//}
+
 @Composable
-fun LoginScreen() {
+fun LoginScreen(onClick:()->Unit) {
     val gradientBrush = Brush.verticalGradient(
         colors = listOf(Color(0xFF6A11CB), Color(0xFF2575FC))
     )
@@ -39,9 +59,45 @@ fun LoginScreen() {
             .fillMaxSize()
             .background(gradientBrush),
         contentAlignment = Alignment.Center
-    ) {
+    )
+    {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+       val ert:Int =673;
+            val convert = ert.toFloat();
+
+          val a:Int? = 1;
+            val plusoperator ='+';
+            val minusOperator = '-'
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Color.Magenta, fontSize = 48.sp, fontWeight = W500)){
+                        append("India")
+                        append(convert.toString());
+                    }
+                }
+            )
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Color.White, fontSize = 48.sp, fontWeight = W200 )){
+                        append("Welcome")
+                    }
+                }
+            )
+
+        }
+
+
+
 
     }
+
+}
+
+fun Button(function: () -> Unit): Button {
+    TODO("Not yet implemented")
 }
 
 
@@ -49,6 +105,6 @@ fun LoginScreen() {
 @Composable
 fun LoginScreenPreview() {
     PracticekotlinTheme {
-        LoginScreen()
+        LoginScreen({})
     }
 }
