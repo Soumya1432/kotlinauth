751543c8cbb209f63ba395de69f77cb50d79065e
IamSoumyaBiswas <imsoumyabiswas@gmail.com>
2025-07-15 21:36:49 +0530

    update

--- app/src/main/java/com/example/practicekotlin/MainActivity.kt
+++ app/src/main/java/com/example/practicekotlin/MainActivity.kt
@@ -5,11 +5,7 @@ import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -18,12 +14,11 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.W200
 import androidx.compose.ui.text.font.FontWeight.Companion.W500
+import androidx.compose.ui.text.font.FontWeight.Companion.W800
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.practicekotlin.ui.theme.PracticekotlinTheme
@@ -37,10 +32,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             PracticekotlinTheme {
                 LoginScreen({})
+                MergedData()
             }
         }
     }
 }
+@Composable
+fun MergedData(){
+    val a:Int = 23;
+    val convetValue = a.toFloat();
+    println(a);
+}
+
+//@Composable
+//fun ArtistCardRow(artist:Artists){
+//    Row(verticalAlignment = Alignment.CenterVertically) {
+//        Image(bitmap = artist.image, contentDescription = "Artist Image")
+//        Column {
+//            Text(artist.name)
+//        }
+//    }
+//}
 
 //@Composable
 //fun FilledButtonExample(onClick: () -> Unit) {
@@ -73,7 +85,7 @@ fun LoginScreen(onClick:()->Unit) {
             val minusOperator = '-'
             Text(
                 buildAnnotatedString {
-                    withStyle(style = SpanStyle(color = Color.Magenta, fontSize = 48.sp, fontWeight = W500)){
+                    withStyle(style = SpanStyle(color = Color.Red, fontSize = 48.sp, fontWeight = W500)){
                         append("India")
                         append(convert.toString());
                     }
@@ -86,6 +98,11 @@ fun LoginScreen(onClick:()->Unit) {
                     }
                 }
             )
+            Text(buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color.Green, fontWeight = W800, fontSize = 34.sp )){
+                    append("First")
+                }
+            })
 
         }
 
